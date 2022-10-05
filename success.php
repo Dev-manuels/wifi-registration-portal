c0a77e8b6348f1dcb78148fb2ff8a9f0a2a09bbf
Manuels-designs <Emmanuel.umeile@gmail.com>
2022-10-05 12:52:43 +0100

    added status checker

--- success.php
+++ success.php
@@ -9,12 +9,12 @@
 <body>
   <?php include 'header.php';?>
   <main>
-  
-    <img class="" src="img/Success.png">
-    <h1 class=""><b>DO NOT RE-REGISTER AGAIN<b></h1>
-    <h1 class="">You will be registered soon</h1>
-    <p class="">NOTE: When logging in<br>Username = Matric No <br>password = Other Name</p>
-    
+    <div class="success-container">
+    <img class="success-img" src="img/Success.png">
+    <h1 class="success-main-text"><b>DO NOT RE-REGISTER AGAIN<b></h1>
+    <h1 class="success-text">You will be registered soon</h1>
+    <p class="success-text">NOTE: When logging in<br>Username = Matric No <br>password = Other Name</p>
+    </div>
   </main>
 
     <?php include 'scripts.php'; ?>
