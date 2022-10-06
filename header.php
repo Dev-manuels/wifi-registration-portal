cc75a6ca2ce75581aaca920fbd798fe70e283333
Manuels-designs <Emmanuel.umeile@gmail.com>
2022-10-06 13:13:14 +0100

    updated structure

--- header.php
+++ header.php
@@ -4,15 +4,28 @@
   <title></title>
 </head>
 <body>
-  <nav>
-        <div class="logo-container">
-          <a href="https://federalpolyilaro.edu.ng"><img class="logo" src="https://federalpolyilaro.edu.ng/images/header-logo.png" alt="FPI logo" height="70px" width="180px"></a>
-        </div>
-        <ul class="main-nav">
-            <li><a class="active" href="index.php">Login</a></li>
-            <li><a href="register.php">Register</a></li>
-        </ul> 
-    </nav>
+  <?php
+      include 'connection.php';
+      session_start();
+          
+      if ($_SESSION['valid'] != true) {
+        echo '
+        <nav>
+          <div class="logo-container">
+            <a href="https://federalpolyilaro.edu.ng"><img class="logo" src="https://federalpolyilaro.edu.ng/images/header-logo.png" alt="FPI logo" height="70px" width="180px"></a>
+          </div>
+          <ul class="main-nav">
+              <li><a class="active" href="index.php">Login</a></li>
+              <li><a href="register.php">Register</a></li>
+          </ul> 
+        </nav>';
+      }else{
+        include 'lheader.php';
+      }
+
+  ?>
+
+  
 
 </body>
 </html>
