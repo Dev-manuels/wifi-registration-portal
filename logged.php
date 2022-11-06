f577c20765f495b7ff5017df9968f80d0426154a
Dev-Manuels <Emmanuel.umeile@gmail.com>
2022-11-06 17:12:56 +0100

    logging status restriction

--- logged.php
+++ logged.php
@@ -1,7 +1,11 @@
+<?php
 session_start();
+
 if ($_SESSION['valid'] != true) {
     $_SESSION['message'] = "Please login!";
     header("Location:index.php");
 }else{
     $_SESSION['message'] = "";
 }
+
+?>
